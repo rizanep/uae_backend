3c486d38217273fa257440fa83ed41fb610e13a2
rizanabaci <rizanabaci@gmail.com>
2026-03-04 16:19:27 +0530

    template

--- Notifications/urls.py
+++ Notifications/urls.py
@@ -2,8 +2,8 @@ from rest_framework.routers import DefaultRouter
 from .views import NotificationViewSet, NotificationTemplateViewSet, BroadcastViewSet
 
 router = DefaultRouter()
-router.register(r'', NotificationViewSet, basename='notifications')
 router.register(r'templates', NotificationTemplateViewSet, basename='notification-templates')
 router.register(r'broadcasts', BroadcastViewSet, basename='broadcasts')
+router.register(r'', NotificationViewSet, basename='notifications')
 
 urlpatterns = router.urls
